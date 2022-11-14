21d050bbe30a4934867294e0097b42ba7dce54ff
ssd04 <dariussuirab@gmail.com>
2022-11-14 12:59:44 +0200

    changed to use key generator

--- mock/keyStub.go
+++ mock/keyStub.go
@@ -23,6 +23,15 @@ type PublicKeyStub struct {
 	SuiteStub       func() crypto.Suite
 }
 
+// KeyGenStub mocks a key generation implementation
+type KeyGenStub struct {
+	GeneratePairStub            func() (crypto.PrivateKey, crypto.PublicKey)
+	PrivateKeyFromByteArrayStub func(b []byte) (crypto.PrivateKey, error)
+	PublicKeyFromByteArrayStub  func(b []byte) (crypto.PublicKey, error)
+	CheckPublicKeyValidStub     func(b []byte) error
+	SuiteStub                   func() crypto.Suite
+}
+
 // ToByteArray returns the byte array representation of the private key
 func (privKey *PrivateKeyStub) ToByteArray() ([]byte, error) {
 	if privKey.ToByteArrayStub != nil {
@@ -90,4 +99,49 @@ func (pubKey *PublicKeyStub) Point() crypto.Point {
 // IsInterfaceNil returns true if there is no value under the interface
 func (pubKey *PublicKeyStub) IsInterfaceNil() bool {
 	return pubKey == nil
+}
+
+// GeneratePair generates a pair of private and public keys
+func (keyGen *KeyGenStub) GeneratePair() (crypto.PrivateKey, crypto.PublicKey) {
+	if keyGen.GeneratePairStub != nil {
+		return keyGen.GeneratePairStub()
+	}
+	return &PrivateKeyStub{}, &PublicKeyStub{}
+}
+
+// PrivateKeyFromByteArray generates the private key from its byte array representation
+func (keyGen *KeyGenStub) PrivateKeyFromByteArray(b []byte) (crypto.PrivateKey, error) {
+	if keyGen.PrivateKeyFromByteArrayStub != nil {
+		return keyGen.PrivateKeyFromByteArrayStub(b)
+	}
+	return &PrivateKeyStub{}, nil
+}
+
+// PublicKeyFromByteArray generates a public key from its byte array representation
+func (keyGen *KeyGenStub) PublicKeyFromByteArray(b []byte) (crypto.PublicKey, error) {
+	if keyGen.PublicKeyFromByteArrayStub != nil {
+		return keyGen.PublicKeyFromByteArrayStub(b)
+	}
+	return &PublicKeyStub{}, nil
+}
+
+// CheckPublicKeyValid verifies the validity of the public key
+func (keyGen *KeyGenStub) CheckPublicKeyValid(b []byte) error {
+	if keyGen.CheckPublicKeyValidStub != nil {
+		return keyGen.CheckPublicKeyValidStub(b)
+	}
+	return nil
+}
+
+// Suite -
+func (keyGen *KeyGenStub) Suite() crypto.Suite {
+	if keyGen.SuiteStub != nil {
+		return keyGen.SuiteStub()
+	}
+	return nil
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (keyGen *KeyGenStub) IsInterfaceNil() bool {
+	return keyGen == nil
 }
