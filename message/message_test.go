d22c6d91da832d91035c768a0ddba6abaad9c7c8
Elrond/ <marius.ciucam@gmail.com>
2023-01-10 12:40:29 +0200

    FIX: Multiversx rebranding

--- message/message_test.go
+++ message/message_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/core/check"
-	"github.com/mutliversx/mx-chain-p2p-go/message"
+	"github.com/multiversx/mx-chain-p2p-go/message"
 	"github.com/stretchr/testify/assert"
 )
 
