7de3df876ed667f80fefde9a465ae07e484769ce
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2023-01-12 13:36:53 +0200

    net messenger refactor part 1 added messages handler and moved the code from net messenger into it extracted maps to a new component topics handler

--- messageCheck/messageVerifier.go
+++ messageCheck/messageVerifier.go
@@ -39,7 +39,7 @@ func NewMessageVerifier(args ArgsMessageVerifier) (*messageVerifier, error) {
 
 func checkArgs(args ArgsMessageVerifier) error {
 	if check.IfNil(args.Marshaller) {
-		return p2p.ErrNilMarshalizer
+		return p2p.ErrNilMarshaller
 	}
 	if args.P2PSigner == nil {
 		return p2p.ErrNilP2PSigner
