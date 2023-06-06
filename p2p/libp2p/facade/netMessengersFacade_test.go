d6ca90299d4a7e91bf91344f9c04da2157d9d2e2
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2023-06-06 10:50:02 +0300

    simplified tests

--- p2p/libp2p/facade/netMessengersFacade_test.go
+++ p2p/libp2p/facade/netMessengersFacade_test.go
@@ -3,40 +3,10 @@ package facade
 import (
 	"testing"
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"github.com/multiversx/mx-chain-communication-go/p2p"
-	"github.com/multiversx/mx-chain-communication-go/p2p/config"
-	"github.com/multiversx/mx-chain-communication-go/p2p/libp2p"
 	"github.com/multiversx/mx-chain-communication-go/p2p/mock"
-	"github.com/multiversx/mx-chain-communication-go/testscommon"
 	"github.com/stretchr/testify/require"
 )
-
-func createMockMessengerArgs() libp2p.ArgsNetworkMessenger {
-	return libp2p.ArgsNetworkMessenger{
-		Marshaller:    &testscommon.ProtoMarshallerMock{},
-		ListenAddress: libp2p.TestListenAddrWithIp4AndTcp,
-		P2pConfig: config.P2PConfig{
-			Node: config.NodeConfig{
-				Port: "0",
-			},
-			KadDhtPeerDiscovery: config.KadDhtPeerDiscoveryConfig{
-				Enabled: false,
-			},
-			Sharding: config.ShardingConfig{
-				Type: p2p.NilListSharder,
-			},
-		},
-		SyncTimer:             &libp2p.LocalSyncTimer{},
-		PreferredPeersHolder:  &mock.PeersHolderStub{},
-		PeersRatingHandler:    &mock.PeersRatingHandlerStub{},
-		ConnectionWatcherType: p2p.ConnectionWatcherTypePrint,
-		P2pPrivateKey:         mock.NewPrivateKeyMock(),
-		P2pSingleSigner:       &mock.SingleSignerStub{},
-		P2pKeyGenerator:       &mock.KeyGenStub{},
-		MessengerType:         p2p.RegularMessenger,
-	}
-}
 
 func TestNewNetworkMessengersFacade(t *testing.T) {
 	t.Parallel()
@@ -51,22 +21,21 @@ func TestNewNetworkMessengersFacade(t *testing.T) {
 	t.Run("nil messenger should error", func(t *testing.T) {
 		t.Parallel()
 
-		netw := mocknet.New()
-		m1, err := libp2p.NewMockMessenger(createMockMessengerArgs(), netw)
-		require.NoError(t, err)
-
-		facade, err := NewNetworkMessengersFacade(m1, nil)
+		facade, err := NewNetworkMessengersFacade(&mock.MessengerStub{}, nil)
 		require.Equal(t, p2p.ErrNilMessenger, err)
 		require.Nil(t, facade)
 	})
-	t.Run("should work", func(t *testing.T) {
+	t.Run("should work with one messenger", func(t *testing.T) {
 		t.Parallel()
 
-		netw := mocknet.New()
-		m1, err := libp2p.NewMockMessenger(createMockMessengerArgs(), netw)
+		facade, err := NewNetworkMessengersFacade(&mock.MessengerStub{})
 		require.NoError(t, err)
+		require.NotNil(t, facade)
+	})
+	t.Run("should work multiple messengers", func(t *testing.T) {
+		t.Parallel()
 
-		facade, err := NewNetworkMessengersFacade(m1)
+		facade, err := NewNetworkMessengersFacade(&mock.MessengerStub{}, &mock.MessengerStub{}, &mock.MessengerStub{})
 		require.NoError(t, err)
 		require.NotNil(t, facade)
 	})
